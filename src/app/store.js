948fe41a0de14a5b429e0cdf6e33b7bd4d6effd9
BC11-ChauPhu <chauphu159951@gmail.com>
2025-04-21 12:19:48 +0700

    update

--- src/app/store.js
+++ src/app/store.js
@@ -5,29 +5,29 @@ import guestSelectReducer from "../features/guestSelection/guestSelectionSlice";
 import sessionStorage from "redux-persist/es/storage/session";
 import dateSelectReducer from "../features/dateSelection/dateSelectSlice";
 import smallHeaderActiveReducer from "../features/smallHeaderAcitve/smallHeaderActiveSlice";
-import averageRatingReducer from '../features/averageRating/averageRatingSlice'
+import averageRatingReducer from "../features/averageRating/averageRatingSlice";
 
 const persistConfig = {
-  key: "root",
-  storage: sessionStorage,
+	key: "root",
+	storage: sessionStorage,
 };
 
 const persistedReducer = persistReducer(persistConfig, registerMapReducer);
 
 export const store = configureStore({
-  reducer: {
-    registerMap: persistedReducer,
-    dateSelection: dateSelectReducer,
-    guestSelection: guestSelectReducer,
-    smallHeaderAcitve: smallHeaderActiveReducer,
-    averageRating: averageRatingReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
-      },
-    }),
+	reducer: {
+		registerMap: persistedReducer,
+		dateSelection: dateSelectReducer,
+		guestSelection: guestSelectReducer,
+		smallHeaderAcitve: smallHeaderActiveReducer,
+		averageRating: averageRatingReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+			},
+		}),
 });
 
 export const persistor = persistStore(store);
